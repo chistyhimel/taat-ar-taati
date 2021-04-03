d5ea2b7ea82809d8c9c92554067d479343c414f0
chistyhimel <fc_himel@yahoo.com>
2021-04-04 05:15:29 +0600

    banner-animation-fixed

--- src/components/Banner/Banner.component.jsx
+++ src/components/Banner/Banner.component.jsx
@@ -5,32 +5,35 @@ import "slick-carousel/slick/slick-theme.css";
 import img1 from "../../assets/images/banner-images/banner-img1.png";
 import img2 from "../../assets/images/banner-images/banner-img2.jpg";
 import img3 from "../../assets/images/banner-images/banner-img3.jpg";
-import { BannerContainer } from "./Banner.style";
+import { BannerContainer, BannerWrapper } from "./Banner.style";
 
 const Banner = () => {
   const settings = {
     dots: false,
+    pauseOnHover: false,
     fade: true,
     infinite: true,
     autoPlaySpeed: 3500,
-    speed: 1500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
   };
   return (
     <>
-      <Slider {...settings}>
-        <BannerContainer>
-          <img src={img1} alt="" />
-        </BannerContainer>
-        <BannerContainer>
-          <img src={img2} alt="" />
-        </BannerContainer>
-        <BannerContainer>
-          <img src={img3} alt="" />
-        </BannerContainer>
-      </Slider>
+      <BannerWrapper>
+        <Slider {...settings}>
+          <BannerContainer>
+            <img src={img1} alt="" />
+          </BannerContainer>
+          <BannerContainer>
+            <img src={img2} alt="" />
+          </BannerContainer>
+          <BannerContainer>
+            <img src={img3} alt="" />
+          </BannerContainer>
+        </Slider>
+      </BannerWrapper>
     </>
   );
 };
--- src/components/Banner/Banner.style.js
+++ src/components/Banner/Banner.style.js
@@ -1,23 +1,50 @@
 import styled from "styled-components";
 
-export const BannerContainer = styled.div`
-  width: 100vw;
-  height: 65vw;
-  @keyframes slideInFromLeft {
-    0% {
-      transform: scale(1.2);
+export const BannerWrapper = styled.div`
+  .slick-active {
+    @keyframes slideInFromLeft {
+      0% {
+        transform: scale(1.2);
+      }
+      100% {
+        transform: scale(1);
+      }
     }
-    100% {
-      transform: scale(1);
+
+    @keyframes fadeInUp {
+      from {
+        opacity: 0;
+        -webkit-transform: translate3d(0, 100%, 0);
+        transform: translate3d(0, 100%, 0);
+      }
+
+      to {
+        opacity: 1;
+        -webkit-transform: none;
+        transform: none;
+      }
+    }
+
+    h1 {
+      opacity: 0;
+
+      animation: fadeInUp 1s ease-in-out 0s forwards;
+    }
+
+    img {
+      transition: all 0.5s;
+      animation: 1s ease-out 0s 1 slideInFromLeft;
     }
   }
+`;
 
+export const BannerContainer = styled.div`
+  width: 100vw;
+  overflow: hidden;
   img {
     width: 100%;
     /* height: 100%; */
     /* object-fit: cover; */
     vertical-align: bottom;
-    transition: all 0.5s;
-    animation: 1s ease-out 0s 1 slideInFromLeft;
   }
 `;
