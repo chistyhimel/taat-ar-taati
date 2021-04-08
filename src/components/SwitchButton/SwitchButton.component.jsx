bf069bb363a43b649c0d3890aa64fda167ee5cb5
chistyhimel <fc_himel@yahoo.com>
2021-04-09 05:35:24 +0600

    switch-icon-fixed

--- src/components/SwitchButton/SwitchButton.component.jsx
+++ src/components/SwitchButton/SwitchButton.component.jsx
@@ -1,3 +1,5 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import Switch from "react-switch";
 import { SwitchContainer } from "./SwitchButton.style";
@@ -23,7 +25,39 @@ const SwitchButton = ({ themeState }) => {
         <Switch
           onChange={handleChange}
           checked={checked}
+          offColor="#000000"
+          onColor="#F2F2F2"
           className="react-switch"
+          uncheckedIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                fontSize: 15,
+                color: "#FFFF",
+                paddingRight: 2,
+              }}
+            >
+              <FontAwesomeIcon icon={faMoon} />
+            </div>
+          }
+          checkedIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                fontSize: 15,
+                color: "orange",
+                paddingRight: 2,
+              }}
+            >
+              <FontAwesomeIcon icon={faSun} />
+            </div>
+          }
         />
       </SwitchContainer>
     </>
